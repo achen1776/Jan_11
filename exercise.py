0bceb8a04dc4dad912175cccf7b0e8d6ea1197ff
Alex Chen <alexander.chen@duke.edu>
2022-01-18 19:18:47 -0500

    Added in the total Cholesterol analysis and tested the code.  Final code tested

--- exercise.py
+++ exercise.py
@@ -7,6 +7,7 @@ def interface():
         print("Options:")
         print("1 - HDL analysis")
         print("2 - LDL analysis")
+        print("3 - Total Cholesterol Analysis")
         print("9 - Quit")
         choice = input("Enter your choice:")
         if choice=='9':
@@ -15,6 +16,8 @@ def interface():
             driver_hdl()
         elif choice=='2':
             driver_ldl()
+        elif choice=='3':
+            driver_total()
 
 def accept_inputs(test_name):
     entry = input("Enter the {} test result: ".format(test_name))
@@ -43,6 +46,20 @@ def check_ldl(entry):
         answer="High"
     else:
         answer="Very High"
+    return answer
+
+def driver_total():
+    entry = accept_inputs("Total Cholesterol")
+    answer = check_total(entry)
+    output(answer)
+
+def check_total(entry):
+    if entry < 200:
+        answer="Normal"
+    elif 239 >= entry >= 200:
+        answer="Borderline High"
+    else:
+        answer="High"
     return answer
 
 def driver_ldl():
